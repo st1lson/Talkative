4fae2a61802f7e30ad6ba979096a936ff58e57bb
Max <rendon4ik@gmail.com>
2022-01-18 13:17:39 +0200

    Add onKeyDown for input

--- Frontend/src/components/InputMessage/InputMessage.jsx
+++ Frontend/src/components/InputMessage/InputMessage.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import classes from './InputMessage.module.scss';
 import { RiSendPlaneFill } from 'react-icons/ri';
 
-const InputMessage = ({ name, onClick, ...otherProps }) => (
-    <>
-        <div className={classes.InputWrapper}>
-            <input autoComplete="off" name={name} {...otherProps} />
-            <button onClick={onClick}>
-                <RiSendPlaneFill />
-            </button>
-        </div>
-    </>
-);
+const InputMessage = ({ name, onClick, ...otherProps }) => {
+    const handleKeyInput = e => {
+        if (e.key === 'Enter') {
+            onClick();
+        }
+    };
+
+    return (
+        <>
+            <div className={classes.InputWrapper}>
+                <input
+                    autoComplete="off"
+                    name={name}
+                    onKeyDown={handleKeyInput}
+                    {...otherProps}
+                />
+                <button onClick={onClick}>
+                    <RiSendPlaneFill />
+                </button>
+            </div>
+        </>
+    );
+};
 
 export default InputMessage;
