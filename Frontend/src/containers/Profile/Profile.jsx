410ca95a91fc15f91bec713dff77e58f2e66efed
rendon4ik <rendon4ik@gmail.com>
2022-07-08 20:56:11 +0300

    Add the option to upload an image

--- Frontend/src/containers/Profile/Profile.jsx
+++ Frontend/src/containers/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Input from '../../components/Input/Input';
 import classes from './Profile.module.scss';
 
@@ -21,6 +21,11 @@ const Profile = props => {
     const [emailInChange, setEmailInChange] = useState(false);
     const [passwordInChange, setPasswordInChange] = useState(false);
 
+    //image
+    const [image, setImage] = useState(null);
+    const [isImageUpdated, setIsImageUpdated] = useState(false);
+    const imageRef = useRef();
+
     return (
         <div className={classes.profileContrainer}>
             <div className={classes.userDataContainer}>
@@ -34,13 +39,16 @@ const Profile = props => {
                 {userNameInChange ? (
                     <div className={classes.buttonsWrapper}>
                         <button
+                            className={classes.button1}
                             onClick={() => {
                                 setUserNameDisabledValue(true);
                                 setUserNameInChange(false);
                             }}>
                             Back
                         </button>
-                        <button onClick={() => console.log('change email')}>
+                        <button
+                            className={classes.button2}
+                            onClick={() => console.log('change email')}>
                             Submit
                         </button>
                     </div>
@@ -63,13 +71,16 @@ const Profile = props => {
                 {emailInChange ? (
                     <div className={classes.buttonsWrapper}>
                         <button
+                            className={classes.button1}
                             onClick={() => {
                                 setEmailDisabledValue(true);
                                 setEmailInChange(false);
                             }}>
                             Back
                         </button>
-                        <button onClick={() => console.log('change email')}>
+                        <button
+                            className={classes.button2}
+                            onClick={() => console.log('change email')}>
                             Submit
                         </button>
                     </div>
@@ -112,12 +123,15 @@ const Profile = props => {
                             onChange={e => setConfirmedPassword(e.target.value)}
                         />
                         <div className={classes.buttonsWrapper}>
-                            <button onClick={() => setPasswordInChange(false)}>
+                            <button
+                                className={classes.button1}
+                                onClick={() => setPasswordInChange(false)}>
                                 Back
                             </button>
                             <button
+                                className={classes.button2}
                                 onClick={() => console.log('change password')}>
-                                Continue
+                                Submit
                             </button>
                         </div>
                     </div>
@@ -127,7 +141,23 @@ const Profile = props => {
                     </button>
                 )}
             </div>
-            <img src={imageUrl} width="400" height="400" />
+            <div className={classes.imageWrapper}>
+                <img src={imageUrl} ref={imageRef} width="600" height="600" />
+                <input
+                    type="file"
+                    onChange={e => {
+                        const image = e.target?.files[0];
+                        setImage(image);
+                        const reader = new FileReader();
+                        reader.addEventListener('load', () => {
+                            imageRef.current.src = reader.result;
+                        });
+                        reader.readAsDataURL(image);
+                        setIsImageUpdated(true);
+                    }}
+                />
+                <button disabled={!isImageUpdated}>Save image</button>
+            </div>
         </div>
     );
 };
